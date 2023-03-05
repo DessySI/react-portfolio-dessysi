b7c2bacccb940d856325317fda431c87fa67718c
DessySI <divanova.docs.manager@gmail.com>
2023-03-05 14:20:56 +0000

    starter

--- src/App.js
+++ src/App.js
@@ -1,20 +1,3 @@
 //Must be your top-level component and render all necessary child components
 
 import React from "react";
-import Navbar from "./components/Navbar";
-import Header from "./components/Header";
-import Card from "./components/Card";
-
-function App() {
-  return (
-    <div>
-      <Header />
-      <Navbar />
-      <Card />
-      <Card />
-      <Card />
-    </div>
-  );
-}
-
-export default App;
